5e1a1d8693601be32e90e8bb01e9f963f0a26887
swdcph.nurse <parichat4.vip@gmail.com>
2026-06-17 09:39:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DEFAULT_GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzlGRp0bvvO1yYXQsCX8eVeQqNqeNhVro6r8E7IQlwVv0ypBi7kK1GtYOcQqsRaL9zl/exec';
+const DEFAULT_GAS_API_URL = 'https://script.google.com/macros/library/d/1H-sXYKFoIqMLMHKZyegeri_JaRzQN517xs3aszHGN_bcZXG0fxN9x66w/13';
 const GAS_API_URL = (window.STERILE_API_URL || document.querySelector('meta[name="gas-api-url"]')?.content || DEFAULT_GAS_API_URL).replace(/\/$/, '');
 const THAI_MONTHS = ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.', 'ก.ค.', 'ส.ค.', 'ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.'];
 
